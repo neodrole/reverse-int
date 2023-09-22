64e4af1d4c416b66b537c1fbb19f93a3c41db6a7
drole <genuines.forever@gmail.com>
2023-09-22 17:47:16 +0400

    feat: add solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if (n<0) {
+    n = n*(-1);
+  }
+
+  let stringNum = String(n);
+  let stringSplit = stringNum.split("");
+  stringNum = stringSplit.reverse().join("");
+  return Number(stringNum);
 }
